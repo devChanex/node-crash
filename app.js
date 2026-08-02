1112d4909b699b6d4c0d8a679dc454dbe9629feb
ccadevida03 <c.cadevida03@gmail.com>
2026-08-02 15:45:21 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = "mongodb://chanex_user:password20267@ac-gaam6wk-shard-00-00.bkl8wy1.mongodb.net:27017,ac-gaam6wk-shard-00-01.bkl8wy1.mongodb.net:27017,ac-gaam6wk-shard-00-02.bkl8wy1.mongodb.net:27017/node-crash?ssl=true&replicaSet=atlas-zun71i-shard-0&authSource=admin&appName=node-crash";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+mongoose.connect(dbURI, clientOptions)
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
